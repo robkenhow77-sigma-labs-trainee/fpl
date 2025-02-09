8dd21e48117183b4f43f3bdfd2a4b2cd9aab0b0d
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2025-02-09 10:19:48 +0000

    fixtures

---
+++ database/fixtures.py
@@ -0,0 +1,32 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+def handle_cookie(driver: webdriver.Chrome) -> None:
+    cookie_button = driver.find_element(By.ID, "onetrust-reject-all-handler")
+    cookie_button.send_keys(Keys.RETURN)
+    sleep(1)
+
+
+def get_fixtures(driver: webdriver.Chrome):
+    teams = driver.find_elements(By.CLASS_NAME, "styles__TeamName-sc-od3kjq-4.eMFDti")
+    fixtures = []
+    for i, team in enumerate(teams):
+        if i % 2 == 0:
+            fixture = []
+            fixture.append(team.text)
+            fixtures.append(fixture)
+        else:
+            fixture.append(team.text)
+    return fixtures
+
+
+if __name__ == "__main__":
+    web_driver = webdriver.Chrome()
+    web_driver.get("https://fantasy.premierleague.com/fixtures/25")
+    sleep(2)
+    handle_cookie(web_driver)
+    fixture_list = get_fixtures(web_driver)
